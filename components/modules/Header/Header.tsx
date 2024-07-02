984aa10055619180dc6e828a32e3d76f165a59ba
LikeKod <i.serebryanoy@yandex.ru>
2024-07-02 18:07:52 +0300

    add_logo+list

--- components/modules/Header/Header.tsx
+++ components/modules/Header/Header.tsx
@@ -1,5 +1,7 @@
 'use client'
 import { useLang } from '@/hooks/useLang'
+import Logo from '@/components/elements/Logo/Logo';
+import Link from 'next/link';
 
 const Header = () => {
   const { lang, translation } = useLang()
@@ -9,6 +11,26 @@ const Header = () => {
         <button className='btn-reset header__burger'>
           {translation[lang].header.menu_btn}
         </button>
+        <div className='header__logo'>
+          <Logo />
+        </div>
+        <ul className='header__links list-reset'>
+          <li className='header__links__item'>
+            <button className='btn-reset header__links__item__btn btn--search'></button>
+          </li>
+          <li className='header__links__item'>
+            <Link href='/favorites' className='header__links__item__btn btn--search--favorites'/>
+          </li>
+          <li className='header__links__item'>
+            <Link href='/comparison' className='header__links__item__btn btn--search--compare'/>
+          </li>
+          <li className='header__links__item'>
+            <Link href='/cart' className='header__links__item__btn btn--search--cart'/>
+          </li>
+          <li className='header__links__item'>
+            <Link href='/profile' className='header__links__item__btn btn--search--profile'/>
+          </li>
+        </ul>
       </div>
     </header>
   )
