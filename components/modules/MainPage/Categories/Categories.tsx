ff964d457ccccb014a0865d69e21128348a10587
LikeKod <i.serebryanoy@yandex.ru>
2024-07-20 22:00:03 +0300

    continue_category

--- components/modules/MainPage/Categories/Categories.tsx
+++ components/modules/MainPage/Categories/Categories.tsx
@@ -1,7 +1,11 @@
 'use client'
 
+import AllLink from '@/components/elements/AllLinks/AllLink'
+import useImagePreloader from '@/hooks/useImagePreloader'
 import { useLang } from '@/hooks/useLang'
 import { useMediaQuery } from '@/hooks/useMediaQuery'
+import Image from '@/node_modules/next/image'
+import Link from '@/node_modules/next/link'
 import img1 from '@/public/img/categories-img-1.png'
 import img2 from '@/public/img/categories-img-2.png'
 import img3 from '@/public/img/categories-img-3.png'
@@ -11,6 +15,8 @@ import styles from '../../../../styles/main-page/index.module.css'
 const Categories = () => {
   const { lang, translation } = useLang()
   const isMedia490 = useMediaQuery(490)
+  const { handleLoadingImageComplete, imgSpinner } = useImagePreloader()
+  const imgSpinnerClass = imgSpinner ? styles.img_loading : ''
 
   const images = [
     { src: img1, id: 1, title: translation[lang].main_page.category_cloth },
@@ -34,7 +40,64 @@ const Categories = () => {
           {translation[lang].main_page.category_title}
         </h2>
         <div className={styles.categories__inner}>
-
+          <AllLink />
+          {!isMedia490 && (
+            <>
+              <Link
+                href='/catalog/cloth'
+                className={`${styles.categories__right} ${styles.categories__img} ${imgSpinnerClass}`}
+              >
+                <Image
+                  src={img1}
+                  alt='Cloth'
+                  className='transition-opacity opacity-0 duration'
+                />
+                <span>{translation[lang].main_page.category_cloth}</span>
+              </Link>
+              <div className={styles.categories__left}>
+                <div className={styles.categories__left__top}>
+                  <Link
+                    href='/catalog/accessories'
+                    className={`${styles.categories__left__top__right} ${styles.categories__img} ${imgSpinnerClass}`}
+                  >
+                    <Image
+                      src={img2}
+                      alt='Accessories'
+                      className='transition-opacity opacity-0 duration'
+                    />
+                    <span>
+                      {translation[lang].main_page.category_accessories}
+                    </span>
+                  </Link>
+                  <Link
+                    href='/catalog/souvenirs'
+                    className={`${styles.categories__left__top__left} ${styles.categories__img} ${imgSpinnerClass}`}
+                  >
+                    <Image
+                      src={img3}
+                      alt='Souvenirs'
+                      className='transition-opacity opacity-0 duration'
+                    />
+                    <span>
+                      {translation[lang].main_page.category_souvenirs}
+                    </span>
+                  </Link>
+                </div>
+                <Link
+                  href='/catalog/office'
+                  className={`${styles.categories__left__bottom} ${styles.categories__img} ${imgSpinnerClass}`}
+                >
+                  <Image
+                    src={img4}
+                    alt='Office'
+                    className='transition-opacity opacity-0 duration'
+                  />
+                  <span>{translation[lang].main_page.category_office}</span>
+                </Link>
+              </div>
+            </>
+          )}
+          {isMedia490 && <MainSlider images={images} />}
         </div>
       </div>
     </section>
---
+++ hooks/useImagePreloader.ts
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+const useImagePreloader = () => {
+  const [imgSpinner, setImgSpinner] = useState(true)
+
+  const handleLoadingImageComplete = async (img: HTMLImageElement) => {
+    img.classList.remove('opacity-0')
+    setImgSpinner(false)
+  }
+
+  return { handleLoadingImageComplete, imgSpinner }
+}
+
+export default useImagePreloader
